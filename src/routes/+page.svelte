14264aaf34243a3695e76c935c57c997f7669718
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-03-27 17:38:26 +1000

    feat/migrate to sveltekit library template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-	import Button from '../components/Button/Button.svelte';
-</script>
-
-<main>
-	<h1>Sve-ui</h1>
-
-	<Button />
-</main>
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
