953d4f5eab044da12c2be89b71db1653ebe8a761
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-03-27 16:56:14 +1000

    feat/meta config for compilation to library setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/main.ts'),
+      name: 'Sve-UI',
+      fileName: 'sve-ui'
+    }
+  },
+  resolve: {
+    extensions: ['.svelte', '.ts', '.js']
+  }
 })
