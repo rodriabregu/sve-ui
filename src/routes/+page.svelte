334588da8ee62da5d240af996eeb855d1034cb2d
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-10 21:49:17 +1000

    feat/update showcase components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,36 @@
 <script>
+	import Box from '$lib/components/Box/Box.svelte';
 	import Button from '$lib/components/Button/Button.svelte';
+	import Center from '$lib/components/Center/Center.svelte';
+	import Circle from '$lib/components/Circle/Circle.svelte';
 	import CodeExample from '$lib/components/CodeExample/CodeExample.svelte';
+	import Flex from '$lib/components/Flex/Flex.svelte';
+	import Spacer from '$lib/components/Spacer/Spacer.svelte';
 </script>
 
 <main>
-	<h1>Sve-UI</h1>
+	<Center>
+		<Box />
+		<Circle bg={'red'} size={'10rem'}>Hola</Circle>
+		<CodeExample>{`import { Button } from 'sve-ui'`}</CodeExample>
 
-	<CodeExample>{`import { Button } from 'sve-ui'`}</CodeExample>
-
-	<Button size="xsm" bg={'#d4d4d4'}>Click me</Button>
-	<Button size="sm">Click me</Button>
-	<Button size="md" color="red">Click me</Button>
-	<Button size="xl" disabled>Click me</Button>
-	<Button size="xxl" color="green">Click me</Button>
-	<Button size="xxxl" onClick={() => alert('Alert for button')}>Click to alert</Button>
+		<Flex p={1} dir="column">
+			<Button size="xsm" bg={'#d4d4d4'}>Click me</Button>
+			<Button size="sm">Click me</Button>
+			<Button size="md" color="red">Click me</Button>
+			<Button size="xl" disabled>Click me</Button>
+			<Spacer size={10} />
+			<Button size="xxl" color="green">Click me</Button>
+			<Spacer size={10} />
+			<Button size="xxxl" onClick={() => alert('Alert for button')}>Click to alert</Button>
+		</Flex>
+	</Center>
 </main>
 
 <style>
 	main {
+		background-color: black;
+		height: 100vh;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
