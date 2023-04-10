df7e5c6588ae454e644c89e2fbdb3c8fe22a7b62
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-10 21:50:19 +1000

    feat/add new square component

---
+++ src/lib/components/Square/Square.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Flex from '../Flex/Flex.svelte';
+
+	export let size = '1rem';
+</script>
+
+<Flex
+	d="flex"
+	align="center"
+	justify="center"
+	color={'white'}
+	width={size}
+	height={size}
+	{...$$restProps}
+>
+	<slot />
+</Flex>
