2e123fdef9539ff9b084f7aecda62997e2d3b0df
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-10 21:49:01 +1000

    feat/update main export with types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,7 @@
 // Reexport your entry components here
 export { default as Button } from './components/Button/Button.svelte';
+export type { ButtonProps } from './components/Button/Button.svelte';
+
 export { default as CodeExample } from './components/CodeExample/CodeExample.svelte';
+export type { CodeExampleProps } from './components/CodeExample/CodeExample.svelte';
+
