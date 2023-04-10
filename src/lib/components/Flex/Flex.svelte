6e73a7ca8f6b2385bb7de4224f51f07619886269
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-10 21:49:46 +1000

    feat/add new flex component

---
+++ src/lib/components/Flex/Flex.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Box from '../Box/Box.svelte';
+
+	export let dir = 'row';
+	export let direction = 'row';
+	export let justify = 'flex-start';
+	export let align = 'stretch';
+	export let wrap = 'nowrap';
+	export let gap = 0;
+	export let d = '';
+	export let display = '';
+	export let style = '';
+</script>
+
+<Box
+	style={`
+    display: ${d || display || 'flex'};
+    flex-direction: ${dir || direction};
+    justify-content: ${justify};
+    align-items: ${align};
+    flex-wrap: ${wrap};
+    gap: ${gap}rem;
+    ${style}
+  `}
+	{...$$restProps}
+>
+	<slot />
+</Box>
---
+++ src/lib/components/Flex/Flex.svelte.d.ts
@@ -0,0 +1,30 @@
+import { SvelteComponentTyped } from "svelte";
+import type { BoxProps } from '../Box/Box.svelte';
+
+declare const __propDef: {
+    props: {
+        [x: string]: any;
+        dir?: string | undefined;
+        direction?: string | undefined;
+        justify?: string | undefined;
+        align?: string | undefined;
+        wrap?: string | undefined;
+        gap?: number | undefined;
+        d?: string | undefined;
+        display?: string | undefined;
+        style?: string | undefined;
+    } & BoxProps;
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        default: {};
+    };
+};
+export type FlexProps = typeof __propDef.props;
+export type FlexEvents = typeof __propDef.events;
+export type FlexSlots = typeof __propDef.slots;
+export default class Flex extends SvelteComponentTyped<FlexProps, FlexEvents, FlexSlots> {
+}
+export { Flex, FlexProps };
