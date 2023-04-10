1abc3cdce228219d562b93b359da37dd33ea19b5
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-10 21:49:57 +1000

    feat/add new center component

---
+++ src/lib/components/Center/Center.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let width = '100%';
+	export let height = '100%';
+</script>
+
+<div
+	style="display: flex; justify-content: center; align-items: center; width: {width}; height: {height}"
+	{...$$restProps}
+>
+	<slot />
+</div>
