732c1cb9bdeea5b0fd5e47efa0977c6518bdaf26
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-11 22:38:32 +1000

    feat/add to exports new components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,3 +5,16 @@ export type { ButtonProps } from './components/Button/Button.svelte';
 export { default as CodeExample } from './components/CodeExample/CodeExample.svelte';
 export type { CodeExampleProps } from './components/CodeExample/CodeExample.svelte';
 
+export { default as Box } from './components/Box/Box.svelte';
+export type { BoxProps } from './components/Box/Box.svelte';
+
+export { default as Flex } from './components/Flex/Flex.svelte';
+export type { FlexProps } from './components/Flex/Flex.svelte';
+
+export { default as Center } from './components/Center/Center.svelte';
+
+export { default as Spacer } from './components/Spacer/Spacer.svelte';
+
+export { default as Square } from './components/Square/Square.svelte';
+
+export { default as Circle } from './components/Circle/Circle.svelte';
