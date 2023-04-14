1ec5dd03cf6043cb9e5e4049b85ec8d86962a5ec
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-14 21:36:55 +1000

    feat/new grid components set

---
+++ src/lib/components/Grid/Grid.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let columns = 3;
+	export let gap = '1rem';
+	export let flow = 'row';
+</script>
+
+<div
+	style="
+    display: grid;
+    grid-template-columns: repeat({columns}, 1fr);
+    grid-gap: {gap};
+    grid-auto-flow: {flow};
+  "
+	{...$$restProps}
+>
+	<slot />
+</div>
---
+++ src/lib/components/GridItem/GridItem.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	export let colStart = 'auto';
+	export let colEnd = 'auto';
+	export let rowStart = 'auto';
+	export let rowEnd = 'auto';
+	export let area = '';
+	export let justifySelf:
+		| 'start'
+		| 'end'
+		| 'center'
+		| 'stretch'
+		| 'auto'
+		| 'inherit'
+		| 'initial'
+		| 'unset'
+		| 'revert'
+		| 'normal'
+		| 'baseline'
+		| 'first baseline'
+		| 'last baseline'
+		| 'self-start'
+		| 'self-end'
+		| 'left'
+		| 'right'
+		| 'safe center'
+		| 'unsafe center'
+		| 'flex-start'
+		| 'flex-end' = 'auto';
+	export let alignSelf:
+		| 'start'
+		| 'end'
+		| 'center'
+		| 'stretch'
+		| 'auto'
+		| 'inherit'
+		| 'initial'
+		| 'unset'
+		| 'revert'
+		| 'normal'
+		| 'baseline'
+		| 'first baseline'
+		| 'last baseline' = 'auto';
+</script>
+
+<div
+	style="
+    grid-column-start: {colStart};
+    grid-column-end: {colEnd};
+    grid-row-start: {rowStart};
+    grid-row-end: {rowEnd};
+    grid-area: {area};
+    justify-self: {justifySelf};
+    align-self: {alignSelf};
+  "
+>
+	<slot />
+</div>
