8b6bf386af91c74c71130f724115b0fadd7fb4ff
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-16 19:30:56 +1000

    chore/demo showcase components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,37 +2,35 @@
 	import Box from '$lib/components/Box/Box.svelte';
 	import Button from '$lib/components/Button/Button.svelte';
 	import Center from '$lib/components/Center/Center.svelte';
-	import Circle from '$lib/components/Circle/Circle.svelte';
-	import CodeExample from '$lib/components/CodeExample/CodeExample.svelte';
-	import Flex from '$lib/components/Flex/Flex.svelte';
-	import Spacer from '$lib/components/Spacer/Spacer.svelte';
+	import Grid from '$lib/components/Grid/Grid.svelte';
+	import GridItem from '$lib/components/GridItem/GridItem.svelte';
+	import Text from '$lib/components/Text/Text.svelte';
 </script>
 
-<main>
-	<Center>
-		<Box />
-		<Circle bg={'red'} size={'10rem'}>Hola</Circle>
-		<CodeExample>{`import { Button } from 'sve-ui'`}</CodeExample>
-
-		<Flex p={1} dir="column">
+<!-- <Center>
+	<Grid columns={2}>
+		<GridItem justifySelf={'center'}>
 			<Button size="xsm" bg={'#d4d4d4'}>Click me</Button>
+		</GridItem>
+		<GridItem justifySelf={'center'}>
 			<Button size="sm">Click me</Button>
-			<Button size="md" color="red">Click me</Button>
-			<Button size="xl" disabled>Click me</Button>
-			<Spacer size={10} />
-			<Button size="xxl" color="green">Click me</Button>
-			<Spacer size={10} />
-			<Button size="xxxl" onClick={() => alert('Alert for button')}>Click to alert</Button>
-		</Flex>
-	</Center>
-</main>
+		</GridItem>
+		<GridItem justifySelf={'center'}>
+			<Button size="sm">Click me</Button>
+		</GridItem>
+		<GridItem justifySelf={'center'}>
+			<Button size="sm">Click me</Button>
+		</GridItem>
+	</Grid>
 
-<style>
-	main {
-		background-color: black;
-		height: 100vh;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-</style>
+	<Text as={'span'} size={'xl'}>Hola</Text>
+</Center> -->
+<!-- <Box
+	display={{
+		base: 'flex',
+		sm: 'block',
+		md: 'grid'
+	}}>Hola</Box
+> -->
+
+<Box />
