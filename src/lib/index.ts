a352cf5af9d802176a890d79599a28c75ef03ba8
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-16 19:31:11 +1000

    feat/update export types in index lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -12,9 +12,19 @@ export { default as Flex } from './components/Flex/Flex.svelte';
 export type { FlexProps } from './components/Flex/Flex.svelte';
 
 export { default as Center } from './components/Center/Center.svelte';
+export type { CenterProps } from './components/Center/Center.svelte';
 
 export { default as Spacer } from './components/Spacer/Spacer.svelte';
+export type { SpacerProps } from './components/Spacer/Spacer.svelte';
 
 export { default as Square } from './components/Square/Square.svelte';
+export type { SquareProps } from './components/Square/Square.svelte';
 
 export { default as Circle } from './components/Circle/Circle.svelte';
+export type { CircleProps } from './components/Circle/Circle.svelte'
+
+export { default as Grid } from './components/Grid/Grid.svelte';
+export type { GridProps } from './components/Grid/Grid.svelte';
+
+export { default as GridItem } from './components/GridItem/GridItem.svelte';
+export type { GridItemProps } from './components/GridItem/GridItem.svelte';
