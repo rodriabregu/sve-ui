ee9522490b5b43cb1d4529d9e1dadb40659cf4ad
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-16 19:29:16 +1000

    feat/new Text component

---
+++ src/lib/components/Text/Text.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { theme } from '$lib/theme';
+
+	export let as:
+		| 'p'
+		| 'span'
+		| 'h1'
+		| 'h2'
+		| 'h3'
+		| 'h4'
+		| 'h5'
+		| 'h6'
+		| 'a'
+		| 'abbr'
+		| 'address'
+		| 'b'
+		| 'bdi'
+		| 'bdo'
+		| 'blockquote'
+		| 'br'
+		| 'var'
+		| 'cite'
+		| 'code'
+		| 'del'
+		| 'dfn'
+		| 'em'
+		| 'i'
+		| 'ins'
+		| 'kbd'
+		| 'mark'
+		| 'meter'
+		| 'pre'
+		| 'q'
+		| 'div' = 'p';
+	export let size = 'md';
+	export let color = 'inherit';
+	export let fontWeight: keyof typeof theme.fontWeights = 'normal';
+	export let fontSize: keyof typeof theme.fontSizes = 'md';
+	export let fontStyle = 'normal';
+	export let textDecoration = 'none';
+	export let letterSpacing: keyof typeof theme.letterSpacings = 'normal';
+	export let lineHeight = 'normal';
+	export let textAlign = 'left';
+</script>
+
+<svelte:element
+	this={as}
+	style="
+    font-size: var(--chakra-fontSizes-{size});
+    color: var(--chakra-colors-{color});
+    font-weight: ${theme.fontWeights[fontWeight]};
+    font-style: {fontStyle};
+    text-decoration: {textDecoration};
+    letter-spacing: {letterSpacing};
+    line-height: {lineHeight};
+    text-align: {textAlign};
+  "
+>
+	<slot />
+</svelte:element>
