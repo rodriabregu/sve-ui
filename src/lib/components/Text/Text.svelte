ecd2feabaf2e807b8d13361506cbb37193b72905
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-17 18:39:15 +1000

    fix/fixing wrong props in style tag

--- src/lib/components/Text/Text.svelte
+++ src/lib/components/Text/Text.svelte
@@ -32,7 +32,7 @@
 		| 'pre'
 		| 'q'
 		| 'div' = 'p';
-	export let color = 'inherit';
+	export let color = 'black';
 	export let fontWeight: keyof typeof theme.fontWeights = 'normal';
 	export let fontSize: keyof typeof theme.fontSizes = 'md';
 	export let fontStyle = 'normal';
@@ -44,16 +44,16 @@
 
 <svelte:element
 	this={as}
-	style="
-    font-size: var(--chakra-fontSizes-{fontSize});
-    color: var(--chakra-colors-{color});
+	style={`
+    font-size: ${theme.fontSizes[fontSize]};
+    color: ${color};
     font-weight: ${theme.fontWeights[fontWeight]};
-    font-style: {fontStyle};
-    text-decoration: {textDecoration};
-    letter-spacing: {letterSpacing};
-    line-height: {lineHeight};
-    text-align: {textAlign};
-  "
+    font-style: ${fontStyle};
+    text-decoration: ${textDecoration};
+    letter-spacing: ${theme.letterSpacings[letterSpacing]};
+    line-height: ${lineHeight};
+    text-align: ${textAlign};
+  `}
 >
 	<slot />
 </svelte:element>
