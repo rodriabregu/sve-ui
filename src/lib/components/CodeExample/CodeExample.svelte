76519e3e4907c4cb8f8af53fd8734550903d84e9
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-04-17 17:58:27 +1000

    feat/new basic state feature in code example comp

--- src/lib/components/CodeExample/CodeExample.svelte
+++ src/lib/components/CodeExample/CodeExample.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
 	export let typeCodeLabel = 'Sve-UI';
+	export let basic: Boolean = false;
 
 	let copied = false;
 	const copyToClipboard = async () => {
@@ -20,35 +21,41 @@
 </script>
 
 <pre class="code-box">
-	<article class="button-copy">
+	{#if basic}
+		<code>
+    	<slot />
+  	</code>
+	{:else}
+		<article class="button-copy">
 		<span>{typeCodeLabel}</span>
 		<button on:click={copyToClipboard}>
 			{#if copied}
-				<span>Copied</span>
-			{:else}
-				<svg
-					stroke="currentColor"
-					fill="none"
-					stroke-width="2"
-					viewBox="0 0 24 24"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="h-4 w-4"
-					height="1em"
-					width="1em"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
-					/><rect x="8" y="2" width="8" height="4" rx="1" ry="1" /></svg
-				>
+					<span>Copied</span>
+				{:else}
+					<svg
+						stroke="currentColor"
+						fill="none"
+						stroke-width="2"
+						viewBox="0 0 24 24"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="h-4 w-4"
+						height="1em"
+						width="1em"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
+						/><rect x="8" y="2" width="8" height="4" rx="1" ry="1" /></svg
+					>
 				<span>Copy</span>
-			{/if}
+				{/if}
 		</button>
 	</article>
 
   <code id="textCode" class={copied ? 'textCode' : ''}>
     <slot />
   </code>
+	{/if}
 </pre>
 
 <style>
